f0fa5fdfcce151263b04a5098f7b90fadad736cc
Gill Shen <gillarete@gmail.com>
2023-07-06 22:45:34 +0800

    Implement multiple chat management

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ const { Configuration, OpenAIApi } = require("openai");
 const sqlite3 = require("sqlite3");
 
 // Set up the chat history database
-const db = new sqlite3.Database("chatlog.sqlite");
+const db = new sqlite3.Database("chat_history.sqlite");
+
 db.serialize(() => {
   db.run("PRAGMA foreign_keys = ON");
 
   db.run(`
     CREATE TABLE IF NOT EXISTS Chat (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT NOT NULL,
-      system_message TEXT DEFAULT '',
-      date_started TEXT DEFAULT NULL
+      title TEXT NOT NULL DEFAULT 'New Chat',
+      system_message TEXT DEFAULT ''
     );
   `);
 
@@ -24,7 +24,7 @@ db.serialize(() => {
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       chat_id INTEGER NOT NULL,
       model TEXT NOT NULL,
-      timestamp INTEGER NOT NULL,
+      created INTEGER NOT NULL,
       parameters TEXT DEFAULT NULL, -- json field
       finish_reason TEXT DEFAULT NULL,
       FOREIGN KEY (chat_id) REFERENCES Chat(id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -41,6 +41,30 @@ db.serialize(() => {
       FOREIGN KEY (request_id) REFERENCES Request(id) ON UPDATE CASCADE ON DELETE CASCADE
     );
   `);
+
+  db.run(`
+    CREATE VIEW IF NOT EXISTS MessageListView AS
+    SELECT
+      Chat.id AS chat_id, 
+      Chat.title AS chat_title, 
+      Chat.system_message, 
+      Request.id AS request_id,
+      Request.model,
+      Request.created AS completion_created,
+      Request.parameters,
+      Request.finish_reason,
+      Prompt.content AS prompt,
+      Completion.content AS completion,
+      Prompt.tokens AS prompt_tokens,
+	    Completion.tokens AS completion_tokens
+    FROM Chat
+      LEFT JOIN Request ON Chat.id = Request.chat_id
+      LEFT JOIN (SELECT request_id, content, tokens FROM Message WHERE role == 'user') AS Prompt
+        ON Request.id = Prompt.request_id
+      LEFT JOIN (SELECT request_id, content, tokens FROM Message WHERE role == 'assistant') AS Completion
+        ON Request.id = Completion.request_id
+    ORDER BY request_id;
+  `);
 });
 
 // Set up for API requests
@@ -50,7 +74,7 @@ const openai = new OpenAIApi(configuration);
 
 const createWindow = () => {
   const window = new BrowserWindow({
-    width: 1100,
+    width: 1150,
     height: 750,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
@@ -86,9 +110,30 @@ app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 
+ipcMain.on("saved-chats-request", async (event) => {
+  db.all("SELECT * FROM MessageListView", [], function (error, rows) {
+    if (error) {
+      event.sender.send("saved-chats-retrieval-failure", error);
+    } else {
+      event.sender.send("saved-chats-ready", rows);
+    }
+  });
+});
+
 ipcMain.on(
-  "request",
-  async (event, { chatId, model, prompt, context, parameters }) => {
+  "api-request",
+  async (
+    event,
+    {
+      chatId = null,
+      chatTitle = "New Chat",
+      systemMessage = "",
+      model,
+      prompt,
+      context,
+      parameters,
+    }
+  ) => {
     const params = { model, messages: [...context], ...parameters };
     params.messages.push({ role: "user", content: prompt });
 
@@ -97,52 +142,50 @@ ipcMain.on(
       event.sender.send("response-ready");
 
       // save to database
-      const timestamp = chatCompletion.data.created * 1000;
+      const timestamp = chatCompletion.data.created;
       const completionContent = chatCompletion.data.choices[0].message.content;
       const finishReason = chatCompletion.data.choices[0].finish_reason;
       const promptTokens = chatCompletion.data.usage.prompt_tokens;
       const completionTokens = chatCompletion.data.usage.completion_tokens;
-      let requestId;
 
-      db.serialize(() => {
-        // TODO handle chat creation
-        db.run(
-          "INSERT OR IGNORE INTO Chat (id, title, system_message, date_started) VALUES (?, ?, ?, ?)",
-          chatId,
-          "New Chat",
-          "",
-          timestamp
-        );
-        db.run(
-          "INSERT INTO Request (chat_id, model, timestamp, parameters, finish_reason) VALUES (?, ?, ?, ?, ?)",
-          chatId,
-          model,
-          timestamp,
-          JSON.stringify(parameters, null, ""),
-          finishReason,
-          function (error) {
-            if (error) {
-              throw error;
-            } else {
-              requestId = this.lastID;
-              saveMessages(
-                requestId,
-                prompt,
-                completionContent,
-                promptTokens,
-                completionTokens
-              );
-            }
-          }
-        );
-      });
+      if (chatId === null) {
+        try {
+          chatId = await execInsert(
+            "INSERT INTO Chat (title, system_message) VALUES (?, ?)",
+            [chatTitle, systemMessage]
+          );
+          event.sender.send("chat-created", {
+            chatId,
+            chatTitle,
+            systemMessage,
+          });
+        } catch (error) {
+          throw error;
+        }
+      }
 
-      event.sender.send("response-success", {
-        requestId,
+      const requestId = await saveCompletion(
+        chatId,
+        model,
         timestamp,
+        parameters,
+        finishReason,
         prompt,
         completionContent,
+        promptTokens,
+        completionTokens
+      );
+
+      // render response
+      event.sender.send("response-success", {
+        chatId,
+        requestId,
+        model,
+        timestamp,
+        parameters,
         finishReason,
+        prompt,
+        completionContent,
         promptTokens,
         completionTokens,
       });
@@ -153,25 +196,51 @@ ipcMain.on(
   }
 );
 
-const saveMessages = (
-  requestId,
+const saveCompletion = async (
+  chatId,
+  model,
+  timestamp,
+  parameters,
+  finishReason,
   prompt,
   completionContent,
   promptTokens,
   completionTokens
 ) => {
+  let requestId;
+  const jsonParams = JSON.stringify(parameters, null, "");
+  try {
+    requestId = await execInsert(
+      "INSERT INTO Request (chat_id, model, created, parameters, finish_reason) VALUES (?, ?, ?, ?, ?)",
+      [chatId, model, timestamp, jsonParams, finishReason]
+    );
+  } catch (error) {
+    throw error;
+  }
+
   db.serialize(() => {
     saveMessage(requestId, "user", prompt, promptTokens);
     saveMessage(requestId, "assistant", completionContent, completionTokens);
+  });
+
+  return requestId;
+};
+
+const execInsert = (query, params) => {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(this.lastID);
+      }
+    });
   });
 };
 
 const saveMessage = (requestId, role, content, tokens) => {
   db.run(
     "INSERT INTO Message (request_id, role, content, tokens) VALUES (?, ?, ?, ?)",
-    requestId,
-    role,
-    content,
-    tokens
+    [requestId, role, content, tokens]
   );
 };
--- preload.js
+++ preload.js
@@ -1,130 +1,37 @@
 const { ipcRenderer } = require("electron");
 const marked = require("marked");
 
-class Chat {
-  constructor(id, name) {
-    this._id = id;
-    this.name = name;
-
-    // two containers of request objects:
-    // {
-    //   requestId,
-    //   timestamp,
-    //   prompt,
-    //   completionContent,
-    //   finishReason,
-    //   promptTokens,
-    //   completionTokens,
-    // }
-    this._history = [];
-    this._context = [];
-  }
-
-  contextArray() {
-    // generate an array of messages that can be used in API requests
-    const arr = [];
-    for (const { prompt, completionContent } of this._context) {
-      arr.push({ role: "user", content: prompt });
-      arr.push({ role: "assistant", content: completionContent });
-    }
-    return arr;
-  }
-
-  appendRequest(request) {
-    this._history.push(request);
-    this._context.push(request);
-  }
-
-  getContextLength() {
-    let sum = 0;
-    for (const { promptTokens, completionTokens } of this._context) {
-      sum += promptTokens + completionTokens;
-    }
-    return sum;
-  }
-
-  trimContext(maximum = 4097, reserve = 410) {
-    while (this.getContextLength() + reserve > maximum) {
-      return this._context.shift();
-    }
-  }
-
-  clearContext() {
-    this._context = [];
-  }
-}
-
-const chats = new Map(); // id -> Chat
-
-// TODO to be created dynamically
-chats.set(1, new Chat(1, "New Chat"));
-
-let activeResponseBox = null;
-let activeAvatar = null;
-
-const sendPrompt = async () => {
-  // Retrieve the prompt as plain text
-  const textarea = document.getElementById("input-textarea");
-  const prompt = textarea.innerText;
-
-  // Create the prompt container
-  const { messageBox: promptBox } = createMessageRow("prompt");
-  promptBox.innerHTML = mdToHtml(prompt);
-
-  // Clear the input area
-  textarea.innerHTML = "";
-
-  // Send the prompt to the main process
-  const chat = chats.get(1); // TODO
-  chat.trimContext();
-  const context = chat.contextArray();
-
-  ipcRenderer.send("request", {
-    chatId: 1, // TODO
-    model: "gpt-3.5-turbo", // TODO get from user input
-    prompt,
-    context,
-    parameters: {}, // TODO get from user input
-  });
-
-  // Display the loading gif
-  const loadingIcon = document.createElement("img");
-  loadingIcon.setAttribute("id", "loading-icon");
-  loadingIcon.setAttribute("src", "assets/loading-dots.gif");
-
-  const { avatar, messageBox: responseBox } = createMessageRow("response");
-  responseBox.appendChild(loadingIcon);
-  responseBox.scrollIntoView();
-
-  activeResponseBox = responseBox;
-  activeAvatar = avatar;
-};
-
 window.addEventListener("DOMContentLoaded", () => {
-  // Send button
+  // ask main.js for a list of chats
+  ipcRenderer.send("saved-chats-request");
+
+  // new chat button
+  const newChatButton = document.getElementById("new-chat-button");
+  newChatButton.addEventListener("click", startNewChat);
+
+  // send button
   const textarea = document.getElementById("input-textarea");
   const sendButton = document.getElementById("send-button");
 
   sendButton.addEventListener("click", sendPrompt);
 
-  // User input keydown event listener (Enter key)
+  // user input keydown event listener (Enter key)
   textarea.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
       if (event.ctrlKey || event.shiftKey) {
         // insert a newline on ctrl+return or shift+return
+        // always effective
         document.execCommand("insertLineBreak");
       } else {
-        sendPrompt(); // send the prompt on a bare return
+        // send the prompt on a bare return
+        // effective only when no API request is in progress
+        sendPrompt();
       }
       event.preventDefault();
     }
-    // if (event.key === "Enter" && event.ctrlKey) {
-    //   sendPrompt();
-    //   event.preventDefault();
-    // }
   });
 
-  // Collapsible sidebar toggle
+  // collapsible sidebar toggle
   const sidebar = document.getElementById("sidebar");
   const sidebarToggle = document.getElementById("sidebar-toggle");
   const toggleIcon = document.getElementById("sidebar-toggle-icon");
@@ -138,7 +45,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // Resizing
+  // resizing
   const resizeHandle = document.getElementById("resize-handle");
   const chatBox = document.getElementById("chat-box");
   const inputBox = document.getElementById("input-box");
@@ -169,9 +76,307 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+const savedChats = new Map(); // id -> Chat
+
+// a variable to hold the active Chat instance;
+// null when a new chat (unsaved) is active
+let activeChat = null;
+
+let activeResponseBox = null;
+let activeAvatar = null;
+let activePrompt = "";
+
+let requestInProgress = false;
+
+class Chat {
+  constructor(id, title = "New Chat", systemMessage = "") {
+    this.id = id;
+    this.title = title;
+    this.systemMessage = systemMessage;
+
+    // two containers of request objects:
+    // {
+    //   requestId,
+    //   model,
+    //   parameters,
+    //   timestamp,
+    //   finishReason,
+    //   prompt,
+    //   completionContent,
+    //   promptTokens,
+    //   completionTokens,
+    // }
+    this._history = [];
+    this._context = [];
+  }
+
+  updated() {
+    if (this._history.length === 0) {
+      return Infinity;
+    }
+    const timestamps = this._history.map((req) => req.timestamp);
+    return Math.max(...timestamps);
+  }
+
+  historyArray() {
+    // generate an array of messages for display in the chat box
+    return this._history.map(({ prompt, completionContent }) => ({
+      prompt,
+      completionContent,
+    }));
+  }
+
+  contextArray(trim = true) {
+    // generate an array of messages for use in API requests
+    if (trim) {
+      this.trimContext();
+    }
+
+    const arr = [];
+    for (const { prompt, completionContent } of this._context) {
+      arr.push({ role: "user", content: prompt });
+      arr.push({ role: "assistant", content: completionContent });
+    }
+    return arr;
+  }
+
+  appendRequest(request) {
+    this._history.push(request);
+    this._context.push(request);
+  }
+
+  getContextLength() {
+    let sum = 0;
+    for (const { promptTokens, completionTokens } of this._context) {
+      sum += promptTokens + completionTokens;
+    }
+    return sum;
+  }
+
+  trimContext(maximum = 4097, reserve = 410) {
+    while (this.getContextLength() + reserve > maximum) {
+      this._context.shift();
+    }
+  }
+
+  clearContext() {
+    this._context = [];
+  }
+}
+
+const loadHistory = (historyArray) => {
+  const chatBox = document.getElementById("chat-box");
+  chatBox.innerHTML = "";
+
+  for (const { prompt, completionContent } of historyArray) {
+    const { messageBox: promptBox } = createMessageRow("prompt");
+    promptBox.innerHTML = mdToHtml(prompt);
+    const { messageBox: responseBox } = createMessageRow("response");
+    responseBox.innerHTML = mdToHtml(completionContent);
+    responseBox.scrollIntoView();
+  }
+};
+
+const setRequestInProgress = (flag) => {
+  // accepts a boolean
+  requestInProgress = flag;
+
+  // if true, disable the sidebar and prompt-sending
+  const sendIcon = document.getElementById("send-icon");
+  const sendButton = document.getElementById("send-button");
+  const newChatButton = document.getElementById("new-chat-button");
+  const chatList = document.getElementById("chat-list");
+
+  if (flag) {
+    sendIcon.setAttribute("src", "assets/send-disabled.svg");
+    sendButton.classList.add("disabled");
+    newChatButton.classList.add("disabled");
+    chatList.classList.add("disabled");
+  } else {
+    sendIcon.setAttribute("src", "assets/send.svg");
+    sendButton.classList.remove("disabled");
+    newChatButton.classList.remove("disabled");
+    chatList.classList.remove("disabled");
+  }
+};
+
+const startNewChat = () => {
+  if (requestInProgress) return;
+
+  activeChat = null;
+  document.getElementById("chat-box").innerHTML = "";
+  const chatList = document.getElementById("chat-list");
+  for (const chatButton of chatList.children) {
+    chatButton.classList.remove("selected");
+  }
+};
+
+const sendPrompt = async () => {
+  if (requestInProgress) return;
+
+  setRequestInProgress(true);
+
+  // retrieve the prompt as plain text
+  const textarea = document.getElementById("input-textarea");
+  const prompt = textarea.innerText;
+
+  // send the prompt to the main process
+  ipcRenderer.send("api-request", {
+    chatId: activeChat?.id ?? null,
+    model: "gpt-3.5-turbo", // TODO get from user input
+    prompt,
+    context: activeChat?.contextArray() ?? [],
+    parameters: {}, // TODO get from user input
+  });
+
+  // create the prompt container
+  const { messageBox: promptBox } = createMessageRow("prompt");
+  promptBox.innerHTML = mdToHtml(prompt);
+
+  // clear the input area
+  textarea.innerHTML = "";
+
+  // display the loading gif
+  const loadingIcon = document.createElement("img");
+  loadingIcon.setAttribute("id", "loading-icon");
+  loadingIcon.setAttribute("src", "assets/loading-dots.gif");
+
+  const { avatar, messageBox: responseBox } = createMessageRow("response");
+  responseBox.appendChild(loadingIcon);
+  responseBox.scrollIntoView();
+
+  activeResponseBox = responseBox;
+  activeAvatar = avatar;
+  activePrompt = prompt;
+};
+
+ipcRenderer.on("saved-chats-retrieval-failure", (_, error) => {
+  throw error; // TODO
+});
+
+ipcRenderer.on("saved-chats-ready", (_, rows) => {
+  let currentChat = null;
+
+  for (const row of rows) {
+    if (savedChats.has(row.chat_id)) {
+      currentChat = savedChats.get(row.chat_id);
+    } else {
+      currentChat = new Chat(row.chat_id, row.chat_title, row.system_message);
+      savedChats.set(row.chat_id, currentChat);
+    }
+    currentChat.appendRequest({
+      requestId: row.request_id,
+      model: row.model,
+      parameters: JSON.parse(row.parameters),
+      timestamp: row.completion_created,
+      finishReason: row.finish_reason,
+      prompt: row.prompt,
+      completionContent: row.completion,
+      promptTokens: row.prompt_tokens,
+      completionTokens: row.completion_tokens,
+    });
+  }
+
+  // list chats in the sidebar, sorted by most-recently-updated
+  const sortedChats = Array.from(savedChats.values()).sort(
+    (a, b) => b.updated() - a.updated()
+  );
+  for (const chat of sortedChats) {
+    createChatButton(chat.id, chat.title);
+  }
+});
+
+const createChatButton = (chatId, chatTitle) => {
+  const chatList = document.getElementById("chat-list");
+
+  const chatButton = document.createElement("button");
+  chatButton.setAttribute("id", `chat-${chatId}`);
+  chatButton.setAttribute("class", "chat-button");
+  chatList.appendChild(chatButton);
+
+  const chatButtonIcon = document.createElement("img");
+  chatButtonIcon.setAttribute("src", "assets/speech-bubble.png");
+  chatButtonIcon.classList.add("chat-button-icon");
+  chatButton.appendChild(chatButtonIcon);
+
+  const chatButtonText = document.createElement("div");
+  chatButtonText.classList.add("chat-button-text");
+  chatButtonText.innerText = chatTitle;
+  chatButton.appendChild(chatButtonText);
+
+  const chatEditButton = document.createElement("button");
+  chatEditButton.classList.add("chat-edit-button");
+  chatButton.appendChild(chatEditButton);
+
+  const chatEditIcon = document.createElement("img");
+  chatEditIcon.classList.add("chat-edit-icon");
+  chatEditIcon.setAttribute("src", "assets/edit-chat.png");
+  chatEditButton.appendChild(chatEditIcon);
+
+  const chatDeleteButton = document.createElement("button");
+  chatDeleteButton.classList.add("chat-delete-button");
+  chatButton.appendChild(chatDeleteButton);
+
+  const chatDeleteIcon = document.createElement("img");
+  chatDeleteIcon.classList.add("chat-delete-icon");
+  chatDeleteIcon.setAttribute("src", "assets/delete-chat.png");
+  chatDeleteButton.appendChild(chatDeleteIcon);
+
+  chatButton.addEventListener("click", () => {
+    // disable chat selection while an API request is in progress
+    if (requestInProgress) return;
+
+    for (const button of chatList.children) {
+      if (button !== chatButton) {
+        button.classList.remove("selected");
+      }
+    }
+    chatButton.classList.add("selected");
+    const selectedChat = savedChats.get(chatId);
+    if (activeChat !== selectedChat) {
+      activeChat = selectedChat;
+      loadHistory(selectedChat.historyArray());
+    }
+  });
+
+  chatEditButton.addEventListener("mouseenter", () => {
+    chatEditIcon.setAttribute("src", "assets/edit-chat-hover.png");
+  });
+  chatEditButton.addEventListener("mouseleave", () => {
+    chatEditIcon.setAttribute("src", "assets/edit-chat.png");
+  });
+  chatEditButton.addEventListener("click", (event) => {
+    // TODO
+    console.log(">>> editing", chatId, chatTitle);
+    event.stopPropagation();
+  });
+
+  chatDeleteButton.addEventListener("mouseenter", () => {
+    chatDeleteIcon.setAttribute("src", "assets/delete-chat-hover.png");
+  });
+  chatDeleteButton.addEventListener("mouseleave", () => {
+    chatDeleteIcon.setAttribute("src", "assets/delete-chat.png");
+  });
+  chatDeleteButton.addEventListener("click", (event) => {
+    // TODO
+    console.log(">>> deleting", chatId, chatTitle);
+    event.stopPropagation();
+  });
+
+  return chatButton;
+};
+
 ipcRenderer.on("response-ready", () => {
-  const loadingIcon = document.getElementById("loading-icon");
-  loadingIcon.remove();
+  setRequestInProgress(false);
+  document.getElementById("loading-icon")?.remove();
+});
+
+ipcRenderer.on("chat-created", (_, { chatId, chatTitle, systemMessage }) => {
+  activeChat = new Chat(chatId, chatTitle, systemMessage);
+  savedChats.set(chatId, activeChat);
+
+  // update sidebar
+  createChatButton(chatId, chatTitle);
 });
 
 ipcRenderer.on(
@@ -179,31 +384,43 @@ ipcRenderer.on(
   (
     _,
     {
+      chatId,
       requestId,
+      model,
       timestamp,
+      parameters,
+      finishReason,
       prompt,
       completionContent,
-      finishReason,
       promptTokens,
       completionTokens,
     }
   ) => {
+    // show completion
     activeResponseBox.innerHTML = mdToHtml(completionContent);
     activeResponseBox.scrollIntoView();
 
-    // TODO use actual chat id
-    chats.get(1).appendRequest({
+    // store request data
+    activeChat.appendRequest({
       requestId,
+      model,
+      parameters,
       timestamp,
+      finishReason,
       prompt,
       completionContent,
-      finishReason,
       promptTokens,
       completionTokens,
     });
 
-    // Update token count
+    // move the related chat button to the top (if it's not there)
+    const chatList = document.getElementById("chat-list");
+    const chatButton = document.getElementById(`chat-${chatId}`);
+    chatList.insertBefore(chatButton, chatList.firstChild);
+
+    // update token count
     const statusBar = document.getElementById("status-bar");
+    // TODO fix
     statusBar.innerText = `${promptTokens + completionTokens} / 4097`;
   }
 );
@@ -214,6 +431,7 @@ ipcRenderer.on("response-error", (_, error) => {
   activeResponseBox.classList.remove("response");
   activeResponseBox.classList.add("error");
   activeResponseBox.innerHTML = error.message;
+  document.getElementById("input-textarea").innerText = activePrompt;
 });
 
 const createMessageRow = (messageType) => {
@@ -239,11 +457,11 @@ const createMessageRow = (messageType) => {
 };
 
 const mdToHtml = (md) => {
-  // for removing zero-width characters, per marked.js documentation
-  const unsafe = /^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/g;
+  const sanitized = md
+    // removing zero-width characters, per marked.js documentation
+    .replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/g, "")
+    // perserving single newlines
+    .replace(/(?<=\S)[ \t]*\n(?=\S)/gm, "  \n");
 
-  // for perserving single newlines
-  const loneNewline = /(?<=\S)[ \t]*\n(?=\S)/gm;
-
-  return marked.parse(md.replace(unsafe, "").replace(loneNewline, "  \n"));
+  return marked.parse(sanitized, { headerIds: false, mangle: false });
 };
